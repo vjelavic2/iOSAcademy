c2e26198e50085f223744767fff9f3d55357f9f9
valentina.jelavic <valentina.jelavic@gmail.com>
2023-12-19 18:42:11 +0100

    Changes to LV3

--- birdy/birdy/ContentView.swift
+++ birdy/birdy/ContentView.swift
@@ -56,7 +56,7 @@ struct ContentView: View {
                 }
                 else{
                     Button(action: {
-                        isPresented=false
+                        userName=""
                     }) {
                         Text("Log out")
                             .foregroundColor(Color.blue)
